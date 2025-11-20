38ebf8abad5c9023f4ec21ab24b88db9d58ce6bc
Vincenzo Fehring <vinfehring@gmail.com>
2025-11-20 14:39:09 -0600

    Add github contribution graph

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import About from "./components/about";
 import Contact from "./components/contact";
 import Experience from "./components/experience";
 import Projects from "./components/projects";
+import GithubCalendar from "react-github-calendar";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="circle-gradient mt-10" />
       <About />
       <Experience />
+      <GithubCalendar username="vinisadev" />
       <Projects />
       <Contact />
     </div>
